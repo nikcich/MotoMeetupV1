ed03df3ec5f58be01f0943c981b2b9f722be082d
nikcich <64556428+nikcich@users.noreply.github.com>
2020-08-12 23:43:33 -0500

    deploy1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+
+
+import './App.css'
+const { v4: uuidv4 } = require('uuid');
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    null
+  )
 }
+
+
+
+
 
 export default App;
